ca0ef3cc02aab5e6c052a1e5fac25d70cc568cd7
Kimhaelyn <81563342+Kimhaelyn@users.noreply.github.com>
2022-02-10 23:40:46 +0900

    Update [07-02] 11720_숫자의 합.py

--- [07-02] 11720_숫자의 합.py
+++ [07-02] 11720_숫자의 합.py
@@ -2,7 +2,7 @@ n=int(input())
 arr=input()
 res=0
 
-for i in range(n): //n개의 숫자를 문자열로 입력받은 뒤 하나씩 분리해서 변수 i로 꺼낸다.
-    res+=int(arr[i]) //문자열을 int 함수로 변환하여 res에 더한다.
+for i in range(n): #n개의 숫자를 문자열로 입력받은 뒤 하나씩 분리해서 변수 i로 꺼낸다.
+    res+=int(arr[i]) #문자열을 int 함수로 변환하여 res에 더한다.
 
 print(res)
