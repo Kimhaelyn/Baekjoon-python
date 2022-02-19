f49c41d9cdc9fafd3c5e9cb9d88821bc0038390d
Kimhaelyn <81563342+Kimhaelyn@users.noreply.github.com>
2022-02-19 19:58:42 +0900

    Add files via upload

---
+++ [16-1] 11047_동전 0.py
@@ -0,0 +1,16 @@
+n, k=map(int, input().split())
+m=[]
+num=0
+
+for i in range(n):
+    m.append(int(input()))
+
+for i in range(n-1, -1, -1): #k보다 작은 값 중 가장 큰 값부터 계산
+    if k==0:
+        break
+    if m[i]>k: 
+        continue
+    num+=k//m[i]
+    k %= m[i]
+
+print(num)
